da1a0737e1e0f1f35069bd60feda311811f49405
Suryavanshi Anant Singh <56921451+suryavanshi1998@users.noreply.github.com>
2020-11-03 00:54:33 +0530

    03.11.2020

---
+++ Page_setup/Page_setup.ino
@@ -0,0 +1,125 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include "arduino_secret.h"
+#include <EEPROM.h>
+
+#include "page1.h"
+#include "page2.h"
+//#include "page3.h"
+//int addr = 0;
+ 
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  Serial.println("GET /");
+  server.send(200, "text/html", htmlPage1);
+}
+
+void handlePage2() {
+  Serial.println("GET /page2");
+  if (server.hasArg("ssid")&& server.hasArg("Password")&& server.hasArg("IP")&&server.hasArg("GW") ) {//If all form fields contain data call handelSubmit()
+    handleSubmit();
+  }
+  else {//Redisplay the form
+  server.send(200, "text/html", htmlPage2);
+}
+}
+void handleSubmit(){//dispaly values and write to memmory
+  String response="<p>The ssid is ";
+ response += server.arg("ssid");
+ response +="<br>";
+ response +="And the password is ";
+ response +=server.arg("Password");
+ response +="<br>";
+ response +="And the IP Address is ";
+ response +=server.arg("IP");
+ response +="</P><BR>";
+ response +="<H2><a href=\"/\">go home</a></H2><br>";
+
+ server.send(200, "text/html", response);
+ //calling function that writes data to memory 
+
+ //calling function that writes data to memory 
+ write_to_Memory(String(server.arg("ssid")),String(server.arg("Password")),String(server.arg("IP")),String(server.arg("GW")));
+}
+//Write data to memory
+/**
+ * We prepping the data strings by adding the end of line symbol I decided to use ";". 
+ * Then we pass it off to the write_EEPROM function to actually write it to memmory
+ */
+void write_to_Memory(String s,String p,String i, String g){
+ s+=";";
+ write_EEPROM(s,0);
+ p+=";";
+ write_EEPROM(p,100);
+ i+=";";
+ write_EEPROM(i,200); 
+ g+=";";
+ write_EEPROM(g,220); 
+ EEPROM.commit();
+}
+//write to memory
+void write_EEPROM(String x,int pos){
+  for(int n=pos;n<x.length()+pos;n++){
+     EEPROM.write(n,x[n-pos]);
+  }
+}
+
+void handleNotFound()
+{
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  message +="<H2><a href=\"/\">go home</a></H2><br>";
+  server.send(404, "text/plain", message);
+} 
+
+
+
+
+
+
+
+/*
+void handlePage3() {
+  Serial.println("GET /page3");
+  server.send(200, "text/html", htmlPage3);
+}
+*/
+void setup(void){
+   EEPROM.begin(512);  //Initialize EEPROM
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  server.on("/page2", handlePage2);
+ // server.on("/page3", handlePage3);
+
+  server.begin();
+  Serial.println("HTTP server started"); 
+}
+
+void loop(void){
+  delay(10);
+  server.handleClient();
+}
---
+++ python projects/pollution_weather_api_export_image_file/text_to_image_final.py
@@ -0,0 +1,102 @@
+from PIL import Image, ImageDraw, ImageFont
+#from json_test import*
+import time
+
+
+def image_air_quality (extract_data ):
+
+	no2= extract_data["NO2"]
+	aqi= extract_data["AQI"]
+	O3= extract_data["O3"]
+	pm2_5= extract_data["PM2_5"]
+	Primary_pollutant = extract_data["primary_pollutant"]
+
+	print("TEST AQI",aqi)
+	# get an image
+	base = Image.open('background.jpg').convert('RGBA')
+
+	# make a blank image for the text, initialized to transparent text color
+	txt = Image.new('RGBA', base.size, (205,205,205,0))
+
+	# get a font
+	#fnt = ImageFont.load("arial.pil")
+	# get a drawing context
+	font1 = ImageFont.truetype(r'REFSAN.ttf', 12) 
+	font2 = ImageFont.truetype(r'REFSAN.ttf', 10) 
+	d = ImageDraw.Draw(txt)
+	d.text((40,2), "Cantonment Board Varanasi",font=font1, fill=(205,205,205,205))
+	d.text((55,15), "Air Quality Parameters",font=font1, fill=(205,205,205,205))
+
+	d.text((20,30), "Primary Pollutant:",font=font1, fill=(205,205,205,205))
+	d.text((138,30), Primary_pollutant,font=font1, fill=(205,205,205,205))
+
+	d.text((20,86), "NO2:",font=font1, fill=(205,205,205,205))
+	d.text((138,86), no2, font=font1,fill=(205,205,205,205))
+
+	d.text((20,68), "O3:",font=font1, fill=(205,205,205,205))
+	d.text((138,68), O3,font=font1, fill=(205,205,205,205))
+
+	d.text((20,104), "AQI:",font=font1, fill=(205,205,205,205))
+	d.text((138,104), aqi,font=font1, fill=(205,205,205,205))
+
+	d.text((20,50), "PM2.5:",font=font1, fill=(205,205,205,205))
+	d.text((138,50), pm2_5,font=font1, fill=(205,205,205,205))
+	
+	out1 = Image.alpha_composite(base, txt)
+	out1.show()
+	out1.save("output_air_quality.png") 
+	#whether_api ()
+
+
+#whether_api ()
+	#return;
+
+
+def image_weather (extract_data):
+
+	tempreture= extract_data["Tempreture"]
+	humidity= extract_data["Humidity"]
+	wind_speed= extract_data["Wind_speed"]
+	wind_direction= extract_data["Wind_direction"]
+
+
+	# get an image
+	base = Image.open('background.jpg').convert('RGBA')
+
+	# make a blank image for the text, initialized to transparent text color
+	txt = Image.new('RGBA', base.size, (205,205,205,0))
+
+	# get a font
+	#fnt = ImageFont.load("arial.pil")
+	# get a drawing context
+	font1 = ImageFont.truetype(r'REFSAN.ttf', 12) 
+	font2 = ImageFont.truetype(r'REFSAN.ttf', 12) 
+	d = ImageDraw.Draw(txt)
+	d.text((40,10), "Cantonment Board Varanasi",font=font1, fill=(205,205,205,205))
+	d.text((40,25), "Ambient Whether Parameters",font=font1, fill=(205,205,205,205))
+
+
+	d.text((20,45), "Tempreture:",font=font2, fill=(205,205,205,205))
+	d.text((130,45), tempreture,font=font2, fill=(205,205,205,205))
+	d.text((150,45), "°C",font=font2, fill=(205,205,205,205))
+
+
+	d.text((20,68), "Humidity:",font=font2, fill=(205,205,205,205))
+	d.text((130,68), humidity,font=font2, fill=(205,205,205,205))
+
+
+
+
+	d.text((20,86), "Wind Speed:",font=font2, fill=(205,205,205,205))
+	d.text((130,86), wind_speed, font=font2,fill=(205,205,205,205))
+
+	d.text((20,104), "Wind Direction:",font=font2, fill=(205,205,205,205))
+	d.text((130,104), wind_direction,font=font2, fill=(205,205,205,205))
+
+
+	out2 = Image.alpha_composite(base, txt)
+
+	out2.show()
+	out2.save("output_weather.png") 
+
+
